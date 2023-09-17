6322e5a45dfc64fd77e8c5be8e7380870595b349
smokingpuddle58 <zzhanggk@connect.ust.hk>
2023-09-17 21:36:38 +0800

    To test my newly written homepage. For testing only.

---
+++ style.js
@@ -0,0 +1,11 @@
+function toggleDark() {
+    let isOn = document.body.classList.toggle("onNight");
+    if (isOn) {
+        document.cookie = "isDark=true";
+        return;
+    }
+    if (!isOn) {
+        document.cookie = "isDark=false";
+        return;
+    }
+}

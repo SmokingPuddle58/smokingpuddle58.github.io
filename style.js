9d588adb157b95505e5665c8c957b0fb48d85199
smokingpuddle58 <zzhanggk@connect.ust.hk>
2023-09-18 22:58:20 +0800

    Updated index.html

--- style.js
+++ style.js
@@ -1,10 +1,14 @@
+// To enable dark mode automatically.
 function toggleDark() {
     let isOn = document.body.classList.toggle("onNight");
     if (isOn) {
         document.cookie = "isDark=true";
-        return;
-    }
-    if (!isOn) {
+    } else {
         document.cookie = "isDark=false";
     }
 }
+
+function getEmail() {
+    window.alert(atob(atob(atob(atob("V1ZjeFIyUlhWblJoUjJocFlsZE9NMVJyVWtaTlJrWklUMVJHYTFJemFESlpha3A2WkZacmVVOVlVUT09")))));
+}
+

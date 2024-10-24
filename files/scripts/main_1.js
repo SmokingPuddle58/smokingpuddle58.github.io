d0f193cad093004b568e5f3b72c93058d8e042d6
SmokingPuddle58 <89989565+SmokingPuddle58@users.noreply.github.com>
2024-10-25 01:26:44 +0800

    feat: Modified page to new version

---
+++ files/scripts/main_1.js
@@ -0,0 +1,13 @@
+function togglePage(oldPage, newPage) {
+    document.getElementById(oldPage).style.display = "none";
+    document.getElementById(newPage).style.display = "block";
+}
+
+function copyEmail() {
+    navigator.clipboard.writeText(atob(atob(atob("WVcxR2RXVnRhR2hpYldOM1RrUkZNRkZIT1RGa1IzaDJZakp6ZFZreU9YUT0="))));
+}
+
+function changeEmoji() {
+    const fourOhFourArray = ['冇', '沒', '無', '空', '虛'];
+    document.getElementById("fourOhFour").textContent = fourOhFourArray[Math.floor(Math.random() * fourOhFourArray.length)];
+}

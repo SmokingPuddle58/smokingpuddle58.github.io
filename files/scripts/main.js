53b58f7910c0900ffb4aae6c3f733b2fa5c17180
SmokingPuddle58 <89989565+SmokingPuddle58@users.noreply.github.com>
2024-10-27 01:47:35 +0800

    feat: Save the dark mode

--- files/scripts/main.js
+++ files/scripts/main.js
@@ -18,7 +18,26 @@ function changeEmoji() {
     document.getElementById("fourOhFour").textContent = fourOhFourArray[Math.floor(Math.random() * fourOhFourArray.length)];
 }
 
-function toggleDark() {
+
+// On page load set the theme.
+(function() {
+    let onpageLoad = localStorage.getItem("theme") || "";
+    let element = document.body;
+    element.classList.add(onpageLoad);
+    document.getElementById("theme").textContent =
+        localStorage.getItem("theme") || "light";
+})();
+
+function themeToggle() {
     let element = document.body;
     element.classList.toggle("dark-mode");
+
+    let theme = localStorage.getItem("theme");
+    if (theme && theme === "dark-mode") {
+        localStorage.setItem("theme", "");
+    } else {
+        localStorage.setItem("theme", "dark-mode");
+    }
+
+    document.getElementById("theme").textContent = localStorage.getItem("theme");
 }
